91abc5687571785cac3db6d52760d7edba0abb87
analopesdev <analopesdemorais@outlook.com>
2020-07-14 20:15:54 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+import 'ui/home_page.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: HomePage(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
